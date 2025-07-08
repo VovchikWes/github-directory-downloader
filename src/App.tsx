e9a73bf3a073fffc6856e35144986663743105d1
VovchikWes <mr.bz@list.ru>
2025-07-08 12:04:52 +0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
   return (
     <div>
       <GithubCorner /> 
-
+      <header className="header">No content is hosted on this website. This is a tool to download files from a user-provided GitHub repository URL.</header>
       <h1>GitHub Directory Downloader</h1>
       <RepoInput onRepoSubmit={(owner, repo, path) => setRepoInfo({ owner, repo, path })} />
       {repoInfo && <FileTree owner={repoInfo.owner} repo={repoInfo.repo} path={repoInfo.path} />}
